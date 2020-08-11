9d23bac5d6f04291c8e489b071fe947bf6540bc0
izichtl <izichtl@gmail.com>
2020-08-10 22:33:07 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const detalColor = document.getElementById('setColor')
+const detalColor1 = document.getElementById('setColor1')
+const detalColor2 = document.getElementById('setColor2')
+const detalColor3 = document.getElementById('setColor3')
+const detalColor4 = document.getElementById('setColor4')
+const detalColor5 = document.getElementById('setColor5')
+const detalColor6 = document.getElementById('setColor6')
+const detalColor7 = document.getElementById('setColor7')
+
+
+setColor()
+function setColor() {
+   
+    
+    detalColor.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor1.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor2.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor3.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor4.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor5.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor6.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+    detalColor7.style.backgroundColor =
+    `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`
+
+    
+}
+
+console.log('Olá estranho que visita meu console :)')
